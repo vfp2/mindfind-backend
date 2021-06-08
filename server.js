419d9b236728c1e9accce00709d79572085a4547
Simon Nishi McCorkindale <simon@funx.jp>
2021-06-08 23:59:07 +0800

    Handle CORS properly

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const AthenaExpress = require("athena-express"),
 
 const app = express()
 const port = 3945
+const cors = require('cors');
 const athenaExpressConfig = {
 	aws,
 	s3: "s3://mindfind-commoncrawl",
@@ -96,6 +97,23 @@ app.get('/api/geturls', async (req, res) => {
   };
   res.send(result);
 })
+
+var allowedOrigins = ['http://localhost:3000',
+                      'https://mindfind.net'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
